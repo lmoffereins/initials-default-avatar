5e2ef7581e4100599326463626332d7ac3c67af9
Laurens Offereins <lmoffereins@gmail.com>
2014-03-31 13:20:18 +0200

    Support BuddyPress avatars

--- initials-default-avatar.php
+++ initials-default-avatar.php
@@ -145,20 +145,22 @@ class Initials_Default_Avatar {
 	private function setup_actions() {
 
 		// Avatar
-		add_filter( 'get_avatar',      array( $this, 'get_avatar'      ), 10, 5 );
-		add_filter( 'avatar_defaults', array( $this, 'avatar_defaults' )        );
-
-		// Store previous avatar option
-		add_filter( 'pre_update_option_avatar_default', array( $this, 'save_previous' ), 10, 2 );
+		add_filter( 'get_avatar',                       array( $this, 'get_avatar'          ), 10, 5 );
+		add_filter( 'avatar_defaults',                  array( $this, 'avatar_defaults'     )        );
+		add_filter( 'pre_update_option_avatar_default', array( $this, 'save_previous'       ), 10, 2 );
+		
+		// Buddypress
+		add_filter( 'bp_core_fetch_avatar',             array( $this, 'bp_get_avatar'       ), 10, 9 );
+		add_filter( 'bp_core_fetch_avatar_url',         array( $this, 'bp_get_avatar'       ), 10, 2 );
 
 		// Admin
-		add_action( 'admin_init',               array( $this, 'hook_admin_message'  )        );
-		add_action( 'wp_ajax_' . $this->notice, array( $this, 'admin_store_notice'  )        );
-		add_action( 'admin_enqueue_scripts',    array( $this, 'enqueue_scripts'     )        );
-		add_action( 'plugin_action_links',      array( $this, 'plugin_action_links' ), 10, 2 );
+		add_action( 'admin_init',                       array( $this, 'hook_admin_message'  )        );
+		add_action( 'wp_ajax_' . $this->notice,         array( $this, 'admin_store_notice'  )        );
+		add_action( 'admin_enqueue_scripts',            array( $this, 'enqueue_scripts'     )        );
+		add_action( 'plugin_action_links',              array( $this, 'plugin_action_links' ), 10, 2 );
 
 		// Settings
-		add_action( 'admin_init', array( $this, 'register_settings' ) );
+		add_action( 'admin_init',                       array( $this, 'register_settings'   )        );
 
 		// Deactivation
 		register_deactivation_hook( __FILE__, array( $this, 'deactivation' ) );
@@ -228,13 +230,12 @@ class Initials_Default_Avatar {
 			}
 		}
 
-		// Get user data and placeholder service
+		// Get user data
 		$user_setup = $this->get_user_data( $user_id );
-		$service    = $this->get_current_service();
 
 		// Setup avatar image attributes
-		$class      = $this->get_avatar_class( $service, $user_setup, $size );
-		$src        = $this->get_avatar_src(   $service, $user_setup, $size );
+		$class      = $this->get_avatar_class( $user_setup, $size );
+		$src        = $this->get_avatar_src(   $user_setup, $size );
 
 		/** 
 		 * Inject avatar string with our class and src
@@ -295,12 +296,16 @@ class Initials_Default_Avatar {
 	 *
 	 * @since 1.0.0
 	 * 
-	 * @param array $service Service data
 	 * @param array $user_data User avatar args
 	 * @param int   $size Avatar size
+	 * @param array $service Defaults to current service
 	 * @return string Class
 	 */
-	public function get_avatar_class( $service, $user_data, $size ) {
+	public function get_avatar_class( $user_data, $size = 96, $service = array() ) {
+
+		// Default to current service
+		if ( empty( $service ) )
+			$service = $this->get_current_service();
 		
 		// Collect avatar classes
 		$classes = apply_filters( 'initials_default_avatar_avatar_class', array(
@@ -323,12 +328,16 @@ class Initials_Default_Avatar {
 	 *
 	 * @since 1.0.0
 	 * 
-	 * @param array $service Service data
 	 * @param array $user_data User avatar args
 	 * @param int   $size Avatar size
+	 * @param array $service Defaults to current service
 	 * @return array Src args
 	 */
-	public function get_avatar_src( $service, $user_data, $size ) {
+	public function get_avatar_src( $user_data, $size = 96, $service = array() ) {
+
+		// Default to current service
+		if ( empty( $service ) )
+			$service = $this->get_current_service();
 
 		// Setup default args
 		$args = array(
@@ -511,6 +520,207 @@ class Initials_Default_Avatar {
 		preg_match( $pattern, $string, $matches );
 
 		return apply_filters( 'initials_default_avatar_first_char', $matches[1], $string );
+	}
+
+	/** Buddypress ************************************************************/
+
+	/**
+	 * Return default initials avatar for buddypress avatars
+	 *
+	 * @since 1.1.0
+	 * 
+	 * @param string $avatar Avatar string
+	 * @param array $args
+	 * @return string Avatar
+	 */
+	public function bp_get_avatar( $avatar, $args, $item_id = 0, $avatar_dir = '', $css_id = '', $html_width = '', $html_height = '', $avatar_folder_url = '', $avatar_folder_dir = '' ) {
+		$bp = buddypress();
+
+		// We are not the default avatar, so no need to be here
+		if ( $this->avatar_key != $bp->grav_default->{$args['object']} )
+			return $avatar;
+
+		// Not a gravatar or valid gravatar found
+		if ( false === strpos( $avatar, 'gravatar' ) || $this->is_valid_gravatar( $avatar ) )
+			return $avatar;
+
+		// Get all args
+		extract( $args, EXTR_OVERWRITE );
+
+		// Setup user data if it isn't registered yet
+		if ( ! $this->bp_has_item_data( $item_id, $object ) ) {
+
+			$item = $this->bp_identify_item( $item_id, $object );
+
+			// Filter item name to be based on something else
+			$item_name = apply_filters( 'initials_default_avatar_bp_item_name', $item['item_name'], $item_id );
+
+			// Require item name
+			if ( ! empty( $item_name ) ) {
+				$this->bp_set_item_data( $item_id, $item_name, $object );
+			} else {
+				return $avatar;
+			}
+		}
+
+		// Get user data
+		$item_setup = $this->bp_get_item_data( $item_id, $object );
+
+		// Set size var
+		if ( false != $width ) {
+			$size = $width;
+		} elseif ( 'thumb' == $type ) {
+			$size = bp_core_avatar_thumb_width();
+		} else {
+			$size = bp_core_avatar_full_width();
+		}
+
+		// Return <img>
+		if ( true === $html ) {
+
+			// Build avatar <img>
+			$class  = $this->get_avatar_class( $item_setup, $size );
+			$src    = $this->get_avatar_src(   $item_setup, $size );
+
+			/** 
+			 * Inject avatar string with our class and src
+			 *
+			 * Since we cannot insert an image url with a querystring into the 
+			 * Gravatar's image src default query arg, we just completely rewrite it.
+			 */
+			$avatar = $this->write_avatar( $avatar, compact( 'class', 'src' ) );
+
+			return apply_filters( 'initials_default_avatar_bp_get_avatar', $avatar, $args, $item_id, $avatar_dir, $css_id, $html_width, $html_height, $avatar_folder_url, $avatar_folder_dir );
+
+		// Return url
+		} else {
+
+			// Build avatar src
+			$avatar = $this->get_avatar_src( $item_setup, $size );
+
+			return apply_filters( 'initials_default_avatar_bp_get_avatar_url', $avatar, $args );
+		}
+	}
+
+	/**
+	 * Return avatar object ID and object name
+	 *
+	 * @since 1.1.0
+	 * 
+	 * @param int $item_id Item ID
+	 * @param string $object Object type
+	 * @return array object data
+	 */
+	public function bp_identify_item( $item_id, $object ) {
+
+		// What object are we looking at?
+		switch ( $object ) {
+
+			case 'blog' :
+				$data = array( 'item_id' => $item_id, 'item_name' => get_blog_option( $item_id, 'blogname' ) );
+				break;
+
+			case 'group' :
+				if ( bp_is_active( 'groups' ) ) {
+					$data = array( 'item_id' => $item_id, 'item_name' => bp_get_group_name( groups_get_group( array( 'group_id' => $item_id ) ) ) );
+				} else {
+					$data = false;
+				}
+
+				break;
+
+			case 'user' :
+			default :
+				$data = $this->identify_user( $item_id );
+				$data = array( 'item_id' => $data['user_id'], 'item_name' => $data['user_name'] );
+				break;
+		}
+
+		return apply_filters( 'bp_identify_item', $data, $item_id, $object );
+	}
+
+	/**
+	 * Return whether the given item is in our objects array
+	 *
+	 * @since 1.1.0 
+	 * 
+	 * @param int $item_id Item ID
+	 * @param string $object Object type
+	 * @return bool Item is registered
+	 */
+	public function bp_has_item_data( $item_id, $object ) {
+
+		// What object are we looking at?
+		switch ( $object ) {
+
+			case 'user' :
+				$has = isset( $this->users[$item_id] );
+				break;
+
+			default :
+				$has = isset( $this->{$object}[$item_id] );
+				break;
+		}
+
+		return apply_filters( 'bp_has_item_data', $has, $item_id, $object );
+	}
+
+	/**
+	 * Return the given object item setup
+	 *
+	 * @since 1.1.0
+	 * 
+	 * @param int $item_id Item ID
+	 * @return array Object item setup
+	 */
+	public function bp_get_item_data( $item_id, $object ) {
+
+		// What object are we looking at?
+		switch ( $object ) {
+
+			case 'user' :
+				$data = $this->users[$item_id];
+				break;
+
+			default :
+				$data = $this->{$object}[$item_id];
+				break;
+		}
+
+		return apply_filters( 'bp_get_item_data', $data, $item_id, $object );
+	}
+
+	/**
+	 * Setup object item data for given item
+	 *
+	 * @since 1.1.0
+	 * 
+	 * @param int $item_id Item ID
+	 * @param string $item_name Item name
+	 */
+	public function bp_set_item_data( $item_id, $item_name, $object ) {
+
+		// Could not identify user
+		if ( empty( $item_id ) || empty( $item_name ) )
+			return;
+
+		// Generate user colors
+		$colors  = $this->generate_colors();
+		$initial = $this->get_first_char( $item_name );
+
+		// Setup and filter user avatar data
+		$data = apply_filters( 'initials_default_avatar_bp_item_data', array(
+			'initial' => ucfirst( $initial ),
+			'bgcolor' => $colors['bgcolor'],
+			'color'   => $colors['color']
+		), $item_id, $item_name );
+
+		// Manipulate object type name for users
+		if ( 'user' == $object )
+			$object = 'users';
+
+		// Store data
+		$this->{$object}[$item_id] = $data;
 	}
 
 	/** User ******************************************************************/
@@ -890,7 +1100,7 @@ class Initials_Default_Avatar {
 
 				<div class="avatar-preview" style="float:left; margin-right: 10px;">
 					<?php $user_data = $this->get_user_data( 'avatar', 'avatar' ); ?>
-					<img src="<?php echo $this->get_avatar_src( $s_args, $user_data, 100 ); ?>" class="<?php echo $this->get_avatar_class( $s_args, $user_data, 100 ); ?>" width="100" height="100" />
+					<img src="<?php echo $this->get_avatar_src( $user_data, 100, $s_args ); ?>" class="<?php echo $this->get_avatar_class( $user_data, 100, $s_args ); ?>" width="100" height="100" />
 				</div>
 
 				<?php if ( isset( $s_args['options'] ) ) : ?>
